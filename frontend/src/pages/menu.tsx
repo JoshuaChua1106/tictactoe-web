9061f94d977d60b911f25882b63ee57b673c3b5b
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-06 18:53:53 +1100

    feat (menu): Added title, description and a play button (no navigation) to menu screen with styling

--- frontend/src/pages/menu.tsx
+++ frontend/src/pages/menu.tsx
@@ -5,9 +5,12 @@ function MenuPage() {
 
   return (
     <>
-      <div>
-       <h1>Tic Tac Toe</h1>
-      </div>
+    <div>
+        <h1>Tic Tac Toe</h1>
+        <p>Welcome to ~Cozy~ Tic-Tac-Tae</p>
+        <p>This is a game made by Joshua where you can quickly enter a game of Tic-Tac-Toe in a comfortable and cozy environment</p>
+        <button>Play Game</button>
+    </div>
     </>
   )
 }
