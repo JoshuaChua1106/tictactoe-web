29d222d265cd8413891acc6de2d179b1d7326ee1
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-06 19:28:20 +1100

    feat (css): Consolidated css styling in index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import MenuPage from './pages/menu'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [currentPage, setCurrentPage] = useState('menu');
 
   if (currentPage === 'menu') {
