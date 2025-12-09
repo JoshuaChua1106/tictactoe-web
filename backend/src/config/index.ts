c6e742c7ec76cbe5259749db01192a6ea39d390f
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-09 11:27:47 +1100

    init (backend): Setup index.ts and resolved issues with CommonJS and exports

---
+++ backend/src/config/index.ts
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+export const FRONT_END_URL = process.env.URL || "http://localhost:5173";
---
+++ backend/src/server.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = Number(process.env.PORT) || 3000;
