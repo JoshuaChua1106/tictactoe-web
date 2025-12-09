f7dc88ee294d9d2602604c75eb83c17f6f145056
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-09 23:47:08 +1100

    feat (lobby-manager): Initialized shell

---
+++ backend/src/services/LobbyManager.ts
@@ -0,0 +1,18 @@
+import type { TicTacToeGame } from "./game/TicTacToeGame.js"
+
+export class LobbyManager {
+    // Attributes
+    private playerList: Array<string>;
+    private LobbyList: Array<TicTacToeGame>;
+
+
+
+    // Constructor
+    constructor() {
+
+    }
+
+    // Methods
+
+
+}
