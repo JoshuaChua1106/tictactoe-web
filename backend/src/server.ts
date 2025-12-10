60ee442c3f405c0c57c340a51724ead478ca7839
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-10 23:11:56 +1100

    feat (lobby): Instatiated lobby-manager singleton in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,22 @@ import { createServer } from "http";
 import app from './app.js'
 import { PORT, FRONT_END_URL } from "./config/index.js";
 
-const httpServer = createServer(app);
+import { Server } from "socket.io";
+import { LobbyManager } from "./services/LobbyManager.js";
 
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+// ===== { Setting up LobbyManager Singleton } =====
+const lobbyManager = new LobbyManager();
+
+io.on('connection', (socket) => {
+    console.log(`Player connected: ${socket.id}`);
+
+    lobbyManager.handleNewPlayer(socket);
+})
+
+// ===== { Setting up the backend server } =====
 httpServer.listen(PORT, () => {
 
     console.log(`Server running on http://localhost:${PORT}`);
