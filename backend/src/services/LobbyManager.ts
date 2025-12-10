a72bda501ad9ce132ea387dd0d479b3344297fca
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-10 23:04:56 +1100

    clean (lobby-manager): Cleaned up ordering of functions for better visibility

--- backend/src/services/LobbyManager.ts
+++ backend/src/services/LobbyManager.ts
@@ -33,31 +33,13 @@ export class LobbyManager {
     }
 
 
-    private createGame(player1: Socket, player2: Socket) {
-        const gameId = this.generateGameId();
-        const gameManager = new GameManager(gameId, player1, player2);
-
-        this.gameList.set(gameId, gameManager);
-
-        console.log(`Game ${gameId} created with players ${player1.id} and ${player2.id}`);
-    }
-
-    private checkForMatch() {
-        if (this.waitingList.length >= 2) {
-            const player1 = this.waitingList.shift()!;
-            const player2 = this.waitingList.shift()!;
-        
-            this.createGame(player1, player2);
-        }
-
-    }
 
     // ===== { SOCKET FUNCTIONS } =====
     private addToQueue(socket: Socket) {
         this.waitingList.push(socket);
         console.log(`Player ${socket.id} joined queue`);
 
-        // this.checkForMatch(); // Try to create a game
+        this.checkForMatch(); // Try to create a game
     }
 
     private removeFromQueue(socket: Socket) {
@@ -74,11 +56,30 @@ export class LobbyManager {
     }
     
     
-    // ===== { HELPER FUNCTIONS } =====
+    // ===== { CREATING A MATCH } =====
+    private checkForMatch() {
+        if (this.waitingList.length >= 2) {
+            const player1 = this.waitingList.shift()!;
+            const player2 = this.waitingList.shift()!;
+        
+            this.createGame(player1, player2);
+        }
+    }
+
+    private createGame(player1: Socket, player2: Socket) {
+        const gameId = this.generateGameId();
+        const gameManager = new GameManager(gameId, player1, player2);
+
+        this.gameList.set(gameId, gameManager);
+
+        console.log(`Game ${gameId} created with players ${player1.id} and ${player2.id}`);
+    }
 
     private generateGameId(): string {
         // Creates IDs like: "game_1641234567890_abc123def"
         return `game_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
     }
+
+
 
 }
