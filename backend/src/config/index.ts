43578edb59ebfd68a035abe9ee2691a2ab7ff229
JoshuaChua1106 <19Joshua.Chua@gmail.com>
2025-12-17 18:13:17 +1100

    debug (backend): Adjusted index code to read FRONT_END_URL from compose.yaml in prod runs, or .env in dev runs

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3001;
-export const FRONT_END_URL = process.env.URL || "http://localhost:5173";
+export const FRONT_END_URL = process.env.FRONT_END_URL || "http://localhost:5173";
